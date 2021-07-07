5ceee277131ca418d37e86373580d2da439bffbe
poojakondoju <kondojupoojitha612@gmail.com>
2021-07-07 15:38:06 +0530

    hello

---
+++ src/javaclass/Demo.java
@@ -0,0 +1,11 @@
+package javaclass;
+
+public class Demo {
+	public static void main(String[] args) {
+	
+		System.out.println("car is running");
+	}
+	void breaking() {
+		System.out.println("break applied");
+	}
+	}
--- src/javaclass/Hellojava.java
+++ src/javaclass/Hellojava.java
@@ -2,7 +2,7 @@ package javaclass;
 
 public class Hellojava {
 	public static void main(String[] args) {
-		System.out.println("welcome hello java");
+		System.out.println("welcome hello java world");
 	}
 
 }
