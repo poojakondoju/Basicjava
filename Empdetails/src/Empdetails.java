d9fe266140fd5cf2887c65e7fc0a9e54f99004cc
poojakondoju <kondojupoojitha612@gmail.com>
2021-07-08 17:24:18 +0530

    emp details

--- Empdetails/src/Empdetails.java
+++ Empdetails/src/Empdetails.java
@@ -2,8 +2,8 @@
 public class Empdetails {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
-		double empceck = Math .floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
+		double empcheck = Math .floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME)
 			System.out.println("employee is present");
 		else
 			System.out.println("employee is abscent");
