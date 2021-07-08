74c8185adfcf128ed8ca38e647803599d71b5635
poojakondoju <kondojupoojitha612@gmail.com>
2021-07-08 16:17:43 +0530

    from new branch

---
+++ Empdetails/src/Empdetails.java
@@ -0,0 +1,12 @@
+
+public class Empdetails {
+	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+		double empceck = Math .floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("employee is present");
+		else
+			System.out.println("employee is abscent");
+	}
+
+}
